2ced2cb4408cf6fac78ad84aa454ed1ad90b4a96
Kelli Rockwell <kellirockwell@gmail.com>
2023-12-28 10:44:58 -0700

    Add day 17, part 2 solution

--- 2023/17.ts
+++ 2023/17.ts
@@ -466,3 +466,99 @@ fs.readFile("./2023/17.txt", (err, rawFile) => {
   );
   console.log("minimum heat loss", result);
 });
+
+/* --- Part Two ---
+The crucibles of lava simply aren't large enough to provide an adequate supply of lava to the machine parts factory. Instead, the Elves are going to upgrade to ultra crucibles.
+
+Ultra crucibles are even more difficult to steer than normal crucibles. Not only do they have trouble going in a straight line, but they also have trouble turning!
+
+Once an ultra crucible starts moving in a direction, it needs to move a minimum of four blocks in that direction before it can turn (or even before it can stop at the end). However, it will eventually start to get wobbly: an ultra crucible can move a maximum of ten consecutive blocks without turning.
+
+In the above example, an ultra crucible could follow this path to minimize heat loss:
+
+2>>>>>>>>1323
+32154535v5623
+32552456v4254
+34465858v5452
+45466578v>>>>
+143859879845v
+445787698776v
+363787797965v
+465496798688v
+456467998645v
+122468686556v
+254654888773v
+432267465553v
+In the above example, an ultra crucible would incur the minimum possible heat loss of 94.
+
+Here's another example:
+
+111111111111
+999999999991
+999999999991
+999999999991
+999999999991
+Sadly, an ultra crucible would need to take an unfortunate path like this one:
+
+1>>>>>>>1111
+9999999v9991
+9999999v9991
+9999999v9991
+9999999v>>>>
+This route causes the ultra crucible to incur the minimum possible heat loss of 71.
+
+Directing the ultra crucible from the lava pool to the machine parts factory, what is the least heat loss it can incur? */
+
+const HEAT_LOSS_TEST_CASES_2: { layout: string; minimumHeatLoss: number }[] = [
+  {
+    layout: `2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533`,
+    minimumHeatLoss: 94,
+  },
+  {
+    layout: `111111111111
+999999999991
+999999999991
+999999999991
+999999999991`,
+    minimumHeatLoss: 71,
+  },
+];
+
+// Test cases
+for (const { layout, minimumHeatLoss } of HEAT_LOSS_TEST_CASES_2) {
+  const rows = layout.split("\n");
+  const graph = buildGraph(rows, 10, 4);
+  const result = findMinimumHeatLoss(
+    graph,
+    `(${rows.length - 1},${rows[0]!.length - 1})`
+  );
+  if (result.cost !== minimumHeatLoss) {
+    console.error("❌, expected", minimumHeatLoss, "but got", result);
+  } else {
+    console.log("✅");
+  }
+}
+
+fs.readFile("./2023/17.txt", (err, rawFile) => {
+  if (err) throw err;
+
+  const rows = rawFile.toString().split("\n");
+  const graph = buildGraph(rows, 10, 4);
+  const result = findMinimumHeatLoss(
+    graph,
+    `(${rows.length - 1},${rows[0]!.length - 1})`
+  );
+  console.log("minimum heat loss part 2", result);
+});
